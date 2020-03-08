6099e8f18964791cbf53ce1454dc4163f457572c
dlerch <dlerch@gmail.com>
2020-03-09 00:03:28 +0100

    test

--- test_jpeg.py
+++ test_jpeg.py
@@ -5,12 +5,17 @@ import numpy as np
 from pprint import pprint
 import jpeg_toolbox
 
-
-r = jpeg_toolbox.load('lena.jpg')
+r = jpeg_toolbox.load('lena.jpg', False)
 #pprint(r)
-print(r.keys())
-print(r["image_width"])
+#print(r.keys())
+#print(r["comp_info"])
 #pprint(r["coef_arrays"])
 #np.set_printoptions(threshold=sys.maxsize)
-print(r["coef_arrays"])
+#print(r["coef_arrays"])
+
+jpeg_toolbox.save(r, 'lena2.jpg')
+r1 = jpeg_toolbox.load('lena2.jpg', False)
+
+
+print(r1==r)
 
