001dc5e493de6e85c659c56c80d5709cad9cb294
dlerch <dlerch@gmail.com>
2020-03-09 18:43:14 +0100

    rename

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
 
-setup(name = 'JPEG Toolbox',
+setup(name = 'jpeg-toolbox',
       version = '1.0',
       author="Daniel Lerch",
       author_email="dlerch@gmail.com",
