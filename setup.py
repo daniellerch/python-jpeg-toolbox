8652482773ef0e2c2ad788604e2830a5c167f2a0
dlerch <dlerch@gmail.com>
2020-03-09 18:36:33 +0100

    missing file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from distutils.core import setup, Extension
+
+m = Extension('jpeg_toolbox_extension', sources = ['jpeg_toolbox_extension.c'], libraries = ['jpeg'])
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(name = 'JPEG Toolbox',
+      version = '1.0',
+      author="Daniel Lerch",
+      author_email="dlerch@gmail.com",
+      url="https://github.com/daniellerch/python-jpeg-toolbox",
+      description = 'The JPEG Toolbox for Python',
+      py_modules = ["jpeg_toolbox"],
+      ext_modules = [m])
